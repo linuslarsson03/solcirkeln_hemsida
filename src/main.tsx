7b3f5bd246275278fd8e371ae587e65b0d05dd7a
ziggemlarzzon-creator <zigge.m.larzzon@gmail.com>
2026-01-30 17:15:32 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css";
+import "./index.css"; // Must import Tailwind + custom CSS
 
 createRoot(document.getElementById("root")!).render(
   <HashRouter>
