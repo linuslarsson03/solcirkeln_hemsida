ff126c3b244975c089ee1f2d9d2780c18cfb4c8f
linus larsson <linus.anders.larsson@gmail.com>
2026-02-20 23:08:25 +0100

    Uppdatera base-path för GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/solcirkeln-direct/",
+  base: "/solcirkeln_hemsida/",
 
   server: {
     host: "::",
