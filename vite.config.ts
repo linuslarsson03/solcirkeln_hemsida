53ef0af6a9fb0e16da23896101498d8cc1b796fc
linus larsson <linus.anders.larsson@gmail.com>
2026-02-22 21:31:57 +0100

    Ta bort favicon och hårdkodad domän, förbereder deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/solcirkeln_hemsida/",
+  base: process.env.VITE_BASE_URL || "/",
 
   server: {
     host: "::",
